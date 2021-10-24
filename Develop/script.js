ceccc54572626bc2e87d736e3fd54a2c16772f8b
Fayaz <flalani100@gmail.com>
2021-10-24 18:13:27 -0400

    Updated js function

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,47 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+}
+
+function getUserOptions(){
+
+  var userChoices = {};
+
+    
+    //Prompt for legnth
+    var passwordLegnth = window.prompt("How many characters would you like your password to be? Choose between 8 and 128!");
+    //validate leChth (later)
+    //sign to UserOptions
+    userChoices.passwordLegnth = passwordLegnth;
+
+
+    //Prompt for upperCase
+    var upperCase = confirm("Would you like uppercase characters?");
+    //sign to upperCase
+    userChoices.upperCase = upperCase;
+    
+    //Prompt for lowerCase
+    var lowerCase = window.prompt("Would you like lowercase characters?");
+    //sign to lowerCase
+    userChoices.lowerCase = lowerCase;
+
+    return UserOptions ()
+
+
+    var alphaNumeric = window.prompt("Would you like numeric characters?");
+    var specialCharacter = window.prompt("Would you like special characters?");
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
+  
+  //get user options
+    var UserOptions = getUserOptions();
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -15,3 +51,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
